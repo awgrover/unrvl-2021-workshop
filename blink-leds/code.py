a5e7b73024d2e36cd24a0d015def5c7c385855e9
Alan Grover <awgrover@gmail.com>
2021-10-02 16:08:57 -0400

    add blink-leds

--- blink-leds/code.py
+++ blink-leds/code.py
@@ -1,30 +1,45 @@
 """
-Blink built-in led and neo-pixel.
+Blink built-in led and neo-pixel and external led.
 Blocking.
 """
 
 # libraries
 import time
+import board
+import digitalio
+
 from adafruit_circuitplayground import cp
+from every.every import Every
 
 # globals
 LIGHT_COLOR = ( 255, 0, 0 ) #( red, green, blue ) each 0-255
 OFF = ( 0, 0, 0 )
 
+d1 = digitalio.DigitalInOut(board.D1)
+d1 = digitalio.DigitalInOut(board.D2)
+
+blink_led = Every(150);
+blink_neo = Every(500);
+
+
 # setup
 cp.red_led = False;
 cp.pixels.brightness = 0.6 # 0.0 to 1.0
+cp.pixels[ 1 ] = OFF
+d1.switch_to_output()
+d2.switch_to_output()
 
-# loop
+loop
 while True:
-    # "copypasta" of blink & blink-neo
 
-    cp.red_led = True
-    time.sleep( 0.5 )
-    cp.red_led = False
-    time.sleep( 0.5 )
+    if blink_led():
+        cp.red_led = ! cp.red_led;
+        d1.value = not d1.value; // blink external too
 
-    cp.pixels[ 1 ] = LIGHT_COLOR
-    time.sleep( 0.5 )
-    cp.pixels[ 1 ] = OFF
-    time.sleep( 0.5 )
+    if blink_neo():
+        if ( cp_pixels[ 1 ] == OFF:
+            cp.pixels[ 1 ] = LIGHT_COLOR
+        else:
+            cp.pixels[ 1 ] = OFF
+
+        d2.value = not d2.value; // blink external too
