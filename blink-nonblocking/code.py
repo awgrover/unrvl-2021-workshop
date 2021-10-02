bf57634faf502a9c2b700b93ed3745419a740204
Alan Grover <awgrover@gmail.com>
2021-10-02 15:54:46 -0400

    do every

--- blink-nonblocking/code.py
+++ blink-nonblocking/code.py
@@ -6,25 +6,27 @@ Blocking.
 # libraries
 import time
 from adafruit_circuitplayground import cp
+from every.every import Every
 
 # globals
 LIGHT_COLOR = ( 255, 0, 0 ) #( red, green, blue ) each 0-255
 OFF = ( 0, 0, 0 )
+blink_led = Every(150);
+blink_neo = Every(500);
 
 # setup
 cp.red_led = False;
 cp.pixels.brightness = 0.6 # 0.0 to 1.0
+cp.pixels[ 1 ] = OFF
 
 # loop
 while True:
-    # "copypasta" of blink & blink-neo
 
-    cp.red_led = True
-    time.sleep( 0.5 )
-    cp.red_led = False
-    time.sleep( 0.5 )
+    if blink_led():
+        cp.red_led = ! cp.red_led;
 
-    cp.pixels[ 1 ] = LIGHT_COLOR
-    time.sleep( 0.5 )
-    cp.pixels[ 1 ] = OFF
-    time.sleep( 0.5 )
+    if blink_neo():
+        if ( cp_pixels[ 1 ] == OFF:
+            cp.pixels[ 1 ] = LIGHT_COLOR
+        else:
+            cp.pixels[ 1 ] = OFF
