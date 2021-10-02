edc36e2819ebb90c6fa87ddca8fceaccf001ceb9
Alan Grover <awgrover@gmail.com>
2021-10-02 15:36:34 -0400

    add blink-wrong/code.py

---
+++ blink-wrong/code.py
@@ -0,0 +1,30 @@
+"""
+Blink built-in led and neo-pixel.
+Blocking.
+"""
+
+# libraries
+import time
+from adafruit_circuitplayground import cp
+
+# globals
+LIGHT_COLOR = ( 255, 0, 0 ) #( red, green, blue ) each 0-255
+OFF = ( 0, 0, 0 )
+
+# setup
+cp.red_led = False;
+cp.pixels.brightness = 0.6 # 0.0 to 1.0
+
+# loop
+while True:
+    # "copypasta" of blink & blink-neo
+
+    cp.red_led = True
+    time.sleep( 0.5 )
+    cp.red_led = False
+    time.sleep( 0.5 )
+
+    cp.pixels[ 1 ] = LIGHT_COLOR
+    time.sleep( 0.5 )
+    cp.pixels[ 1 ] = OFF
+    time.sleep( 0.5 )
