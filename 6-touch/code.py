3c50eafa29030f3b09101ea267d9c32eae4b6f50
Alan Grover <awgrover@gmail.com>
2021-10-03 14:30:20 -0400

    add a heartbeat

--- 6-touch/code.py
+++ 6-touch/code.py
@@ -4,11 +4,13 @@ Respond to a touch on A1
 
 # libraries
 import time
+from every.every import Every
 from adafruit_circuitplayground import cp
 
 # globals
 LIGHT_COLOR = ( 255, 0, 0 ) #( red, green, blue ) each 0-255
 OFF = ( 0, 0, 0 )
+heartbeat = Every(1)
 
 # set up
 # no setup for "touch"
@@ -24,6 +26,9 @@ while True:
 
     else:
         cp.pixels[ 1 ] = OFF
+
+    if heartbeat():
+        cp.red_led = not cp.red_led
 
     # slow the loop so we can upload
     time.sleep( 0.01 )
